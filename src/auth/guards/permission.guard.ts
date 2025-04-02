235115db1b0549950bca7152a07ff75fdc752bee
Yeonjun Lee <yeonfish6040@gmail.com>
2025-04-03 08:32:47 +0900

    TODO: scope based permission management

--- src/auth/guards/permission.guard.ts
+++ src/auth/guards/permission.guard.ts
@@ -3,11 +3,11 @@ import { CanActivate, ExecutionContext, mixin } from "@nestjs/common";
 import { PermissionEnum } from "../../common/mapper/permissions";
 import { hasPermission, parsePermission } from "../../common/utils/permission.util";
 
+// TODO: scope based permission management
 export const PermissionGuard = (requiredPermission: number[], or: boolean = false) => {
   class PermissionGuardMixin implements CanActivate {
     canActivate(context: ExecutionContext) {
       const request = context.switchToHttp().getRequest();
-      console.log(parsePermission(request.user.permission));
       return hasPermission(request.user.permission, requiredPermission, or);
     }
   }
