df25fe6651b1a472ac6e454764bc1a359b717702
yeonfish <yeonfish6040@gmail.com>
2025-08-11 19:55:42 +0900

    fix: login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -101,7 +101,7 @@ export class AuthService {
         identifier2: null,
         email: ticketPayload.email,
         picture: ticketPayload.picture,
-        name: `${ticketPayload.family_name}${ticketPayload.given_name}`,
+        name: `${ticketPayload.family_name || ""}${ticketPayload.given_name || ""}`,
       });
     } else loginUser = login.user;
 
