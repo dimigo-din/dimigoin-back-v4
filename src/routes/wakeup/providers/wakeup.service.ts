ce890d0cb0ddce8b34388074faabec8d21eea6e1
yeonfish <yeonfish6040@gmail.com>
2025-08-28 21:55:05 +0900

    fix: error msg

--- src/routes/wakeup/providers/wakeup.service.ts
+++ src/routes/wakeup/providers/wakeup.service.ts
@@ -17,7 +17,8 @@ export class WakeupService {
     const song = await this.wakeupSongHistoryRepository.find({
       where: { date: data.date, gender: data.gender },
     });
-    if (!song) throw new HttpException(ErrorMsg.NoWakeupInDate(), HttpStatus.NOT_FOUND);
+    if (!song || song.length === 0)
+      throw new HttpException(ErrorMsg.NoWakeupInDate(), HttpStatus.NOT_FOUND);
 
     return song[song.length - 1];
   }
