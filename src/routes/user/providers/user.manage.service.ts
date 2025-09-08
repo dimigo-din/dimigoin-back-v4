6d5d1b9180fa9685cecb1534748277ad2f5a6217
yeonfish <yeonfish6040@gmail.com>
2025-09-08 17:50:17 +0900

    Revert "chore: removing render html"
    This reverts commit 6f3e01df11960b3ce5f179d5cfcd2a2106aa971c.

--- src/routes/user/providers/user.manage.service.ts
+++ src/routes/user/providers/user.manage.service.ts
@@ -5,6 +5,7 @@ import { ConfigService } from "@nestjs/config";
 import { InjectRepository } from "@nestjs/typeorm";
 import axios, { AxiosInstance } from "axios";
 import * as bcrypt from "bcrypt";
+import puppeteer from "puppeteer";
 import { Like, Repository } from "typeorm";
 
 import { PermissionType } from "../../../common/mapper/permissions";
@@ -15,6 +16,7 @@ import {
   AddPermissionDTO,
   CreateUserDTO,
   RemovePermissionDTO,
+  RenderHTMLDTO,
   SearchUserDTO,
   SetPermissionDTO,
 } from "../dto";
@@ -175,5 +177,62 @@ export class UserManageService {
     user.permission = numberPermission(...resultPermissions).toString();
 
     return await this.userRepository.save(user);
+  }
+
+  async renderHtml(data: RenderHTMLDTO) {
+    const browser =
+      process.env.NODE_ENV === "dev"
+        ? await puppeteer.launch()
+        : await puppeteer.launch({
+            executablePath: "/usr/bin/chromium-browser",
+            args: ["--disable-gpu", "--no-sandbox", "--js-flags=--noexpose_wasm,--jitless"],
+          });
+    const page = await browser.newPage();
+
+    await page.setContent(
+      `
+    <html>
+      <head>
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap');
+          * {
+            font-family: 'Noto Sans KR', sans-serif !important;
+          }
+          body {
+            margin: 0;
+            padding: 0;
+          }
+          .pdf-container {
+            width: 210mm;
+            height: auto;
+            padding: 10mm 15mm;
+            display: flex;
+            justify-content: center;
+          }
+          .pdf-inner {
+            width: fit-content;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="pdf-container">
+          <div class="pdf-inner">
+            ${data.html}
+          </div>
+        </div>
+      </body>
+    </html>
+  `,
+      { waitUntil: "networkidle0" },
+    );
+
+    const buffer = await page.pdf({
+      format: "A4",
+      printBackground: true,
+    });
+
+    await browser.close();
+
+    return buffer;
   }
 }
