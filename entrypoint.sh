822fc34beec6b5e79fb4be12cd55e47867a53e77
yeonfish <yeonfish6040@gmail.com>
2025-10-19 20:02:30 +0900

    fix: doppler flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-npm run migration:prod
-doppler run -c $(cat deploy_type) -e $(cat doppler_token) -- pm2 start dist/src/main.js --name dimigoin-back -i ${PM2_INSTANCES:-12}
+doppler run -c $(cat deploy_type) -t $(cat doppler_token) -- npm run migration:prod
+doppler run -c $(cat deploy_type) -t $(cat doppler_token) -- pm2 start dist/src/main.js --name dimigoin-back -i ${PM2_INSTANCES:-12}
 pm2 logs dimigoin-back
