cf02a33aa974c0676dda20c3dbad503869f525cb
shimaenaga <admin@shimaenaga.dev>
2026-01-05 21:29:48 +0900

    fix: type in PersonalInformationVerifyTokenAuthGuard

--- src/auth/guards/personalInformationVerifyToken.guard.ts
+++ src/auth/guards/personalInformationVerifyToken.guard.ts
@@ -1,11 +1,12 @@
 import { type ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import type { Observable } from 'rxjs';
 
 @Injectable()
 export class PersonalInformationVerifyTokenAuthGuard extends AuthGuard(
   'personalInformationVerifyToken',
 ) {
-  canActivate(context: ExecutionContext): boolean {
-    return !!super.canActivate(context);
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    return super.canActivate(context);
   }
 }
