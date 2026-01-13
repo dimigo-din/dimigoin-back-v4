46ddd1e4ae31965b5eb826fda8402534a9792ea4
shimaenaga <admin@shimaenaga.dev>
2026-01-13 17:14:39 +0900

    refactor: DayNumber2String using array

--- src/common/utils/date.util.ts
+++ src/common/utils/date.util.ts
@@ -1,24 +1,8 @@
+const DAYS = ["일", "월", "화", "수", "목", "금", "토"] as const;
+
 export const DayNumber2String = (day: number): string => {
-  if (day === 0) {
-    return "일";
+  if (day < 0 || day > 6) {
+    throw Error("Invalid Range.");
   }
-  if (day === 1) {
-    return "월";
-  }
-  if (day === 2) {
-    return "화";
-  }
-  if (day === 3) {
-    return "수";
-  }
-  if (day === 4) {
-    return "목";
-  }
-  if (day === 5) {
-    return "금";
-  }
-  if (day === 6) {
-    return "토";
-  }
-  throw Error("Invalid Range.");
+  return DAYS[day] ?? "Invalid Day";
 };
