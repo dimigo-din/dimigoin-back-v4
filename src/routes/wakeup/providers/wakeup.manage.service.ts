3a88ad29f5bdc97cd3edcd3dad123344365c6e1f
shimaenaga <admin@shimaenaga.dev>
2026-03-29 10:03:34 +0900

    fix: timezone in wakeup manage

--- src/routes/wakeup/providers/wakeup.manage.service.ts
+++ src/routes/wakeup/providers/wakeup.manage.service.ts
@@ -8,13 +8,14 @@ import { findOrThrow } from "$utils/findOrThrow.util";
 import { softDelete } from "$utils/softDelete.util";
 import { andWhere } from "$utils/where.util";
 import { WakeupSongDeleteDTO, WakeupSongSelectDTO } from "~wakeup/dto/wakeup.manage.dto";
+import { TZDate } from "@date-fns/tz";
 
 @Injectable()
 export class WakeupManageService {
   constructor(@Inject(DRIZZLE) private readonly db: DrizzleDB) {}
 
   async getList() {
-    const week = format(startOfWeek(new Date()), "yyyy-MM-dd");
+    const week = format(startOfWeek(new TZDate(Date(), "Asia/Seoul")), "yyyy-MM-dd");
 
     return await this.db.query.wakeupSongApplication.findMany({
       where: {
@@ -36,7 +37,7 @@ export class WakeupManageService {
 
     // Create history record from the application
     await this.db.insert(wakeupSongHistory).values({
-      date: format(new Date(), "yyyy-MM-dd"),
+      date: format(new TZDate(Date(), "Asia/Seoul"), "yyyy-MM-dd"),
       video_id: apply.video_id,
       video_title: apply.video_title,
       up: apply.wakeupSongVote.filter((v: { upvote: boolean }) => v.upvote).length,
